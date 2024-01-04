1fe96c75f45294293618f57e78dfa2e7c5f42709
Supereuro <bebrez@gmx.de>
2024-01-04 09:27:31 +0100

    Update page.tsx

--- apps/web/src/app/[locale]/(auth)/login/page.tsx
+++ apps/web/src/app/[locale]/(auth)/login/page.tsx
@@ -20,16 +20,6 @@ export default async function LoginPage({ params }: { params: Params }) {
           oidcConfig={isOIDCEnabled ? { name: oidcName } : undefined}
         />
       </AuthCard>
-      <div className="mt-4 text-center pt-4 text-gray-500 sm:text-base">
-        <Trans
-          t={t}
-          i18nKey="notRegistered"
-          defaults="Don't have an account? <a>Register</a>"
-          components={{
-            a: <Link href="/register" className="text-link" />,
-          }}
-        />
-      </div>
     </div>
   );
 }
