5b669b44342c89ee77e2481568afe7b5a3ecb859
Supereuro <bebrez@gmx.de>
2024-01-04 09:52:38 +0100

    Update login-form.tsx

--- apps/web/src/app/[locale]/(auth)/login/login-form.tsx
+++ apps/web/src/app/[locale]/(auth)/login/login-form.tsx
@@ -10,36 +10,35 @@ import { useForm } from "react-hook-form";
 import { Trans, useTranslation } from "react-i18next";
 
 import { trpc } from "@/app/providers";
-import { VerifyCode, verifyCode } from "@/components/auth/auth-forms";
+//import { VerifyCode, verifyCode } from "@/components/auth/auth-forms";
 import { TextInput } from "@/components/text-input";
-import { IfCloudHosted } from "@/contexts/environment";
-import { isSelfHosted } from "@/utils/constants";
-import { validEmail } from "@/utils/form-validation";
+//import { IfCloudHosted } from "@/contexts/environment";
+//import { isSelfHosted } from "@/utils/constants";
+//import { validEmail } from "@/utils/form-validation";
 
 export function LoginForm({ oidcConfig }: { oidcConfig?: { name: string } }) {
   const { t } = useTranslation();
   const session = useSession();
   const queryClient = trpc.useUtils();
-  const [email, setEmail] = React.useState<string>();
+  //const [email, setEmail] = React.useState<string>();
   const posthog = usePostHog();
   const router = useRouter();
   const callbackUrl = (useSearchParams()?.get("callbackUrl") as string) ?? "/";
 
   return (    
-      <div className="mb-1 text-2xl font-bold">{t("login")}</div>
-        <div className="flex flex-col gap-2">
-            <div className="grid gap-4">
-                <Button
-                  icon={LogInIcon}
-                  size="lg"
-                  onClick={() => signIn("oidc")}
-                >
-                  <Trans
-                    i18nKey="loginWith"
-                    values={{ provider: oidcConfig.name }}
-                  />
-                </Button>              
-              </div>
-      </div>
+      <div className="flex flex-col gap-2">
+          <div className="grid gap-4">
+              <Button
+                icon={LogInIcon}
+                size="lg"
+                onClick={() => signIn("oidc")}
+              >
+                <Trans
+                  i18nKey="loginWith"
+                  values={{ provider: oidcConfig.name }}
+                />
+              </Button>              
+            </div>
+    </div>
   );
 }
