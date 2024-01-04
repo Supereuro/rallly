f92f82cf827b7a8e4b2008477b997db4ad22edf4
Supereuro <bebrez@gmx.de>
2024-01-04 09:23:42 +0100

    Update login-form.tsx

--- apps/web/src/app/[locale]/(auth)/login/login-form.tsx
+++ apps/web/src/app/[locale]/(auth)/login/login-form.tsx
@@ -35,98 +35,9 @@ export function LoginForm({ oidcConfig }: { oidcConfig?: { name: string } }) {
   const hasOIDCProvider = !!oidcConfig;
   const allowGuestAccess = !isSelfHosted;
   const hasAlternativeLoginMethods = hasOIDCProvider || allowGuestAccess;
-  const sendVerificationEmail = (email: string) => {
-    return signIn("email", {
-      redirect: false,
-      email,
-      callbackUrl,
-    });
-  };
-
-  if (email) {
-    return (
-      <VerifyCode
-        onSubmit={async (code) => {
-          const success = await verifyCode({
-            email,
-            token: code,
-          });
-          if (!success) {
-            throw new Error("Failed to authenticate user");
-          } else {
-            queryClient.invalidate();
-            const s = await session.update();
-            if (s?.user) {
-              posthog?.identify(s.user.id, {
-                email: s.user.email,
-                name: s.user.name,
-              });
-            }
-            posthog?.capture("login", {
-              method: "verification-code",
-            });
-            router.push(callbackUrl);
-          }
-        }}
-        email={getValues("email")}
-        onChangeEmail={() => {
-          setEmail(undefined);
-        }}
-      />
-    );
-  }
 
   return (
-    <!--<form
-      onSubmit={handleSubmit(async ({ email }) => {
-        const res = await sendVerificationEmail(email);
-
-        if (res?.error) {
-          setError("email", {
-            message: t("userNotFound"),
-          });
-        } else {
-          setEmail(email);
-        }
-      })}
-    >-->
       <div className="mb-1 text-2xl font-bold">{t("login")}</div>
-      <!--<p className="mb-4 text-gray-500">
-        {t("stepSummary", {
-          current: 1,
-          total: 2,
-        })}
-      </p>
-      <fieldset className="mb-4">
-        <label htmlFor="email" className="mb-1 text-gray-500">
-          {t("email")}
-        </label>
-        <TextInput
-          className="w-full"
-          id="email"
-          proportions="lg"
-          autoFocus={true}
-          error={!!formState.errors.email}
-          disabled={formState.isSubmitting}
-          placeholder={t("emailPlaceholder")}
-          {...register("email", { validate: validEmail })}
-        />
-        {formState.errors.email?.message ? (
-          <div className="mt-2 text-sm text-rose-500">
-            {formState.errors.email.message}
-          </div>
-        ) : null}
-      </fieldset>
-      <div className="flex flex-col gap-2">
-        <Button
-          loading={formState.isSubmitting}
-          type="submit"
-          size="lg"
-          variant="primary"
-          className=""
-        >
-          {t("continue")}
-        </Button>-->
         {hasAlternativeLoginMethods ? (
           <>
             <hr className="border-t border-grey-500 my-4" />
